1240604c085cfc05c7aa6ab076f5c6501646fd3f
WWZ <33089305+wangwenzhiwwz@users.noreply.github.com>
2025-10-24 11:28:54 +0800

    Add files via upload

--- apps.js
+++ apps.js
@@ -562,6 +562,18 @@ const appData = [
                 "description": "Videos, creators & friends",
                 "link": "https://apps.apple.com/us/app/instagram/id389801252",
                 "icon": "./images/Instagram.webp"
+            },
+            {
+                "name": "涂鸦",
+                "description": "涂鸦智能App 智生活 能享受",
+                "link": "https://apps.apple.com/cn/app/%E6%B6%82%E9%B8%A6/id1586116399",
+                "icon": "./images/Tuya.webp"
+            },
+            {
+                "name": "米家",
+                "description": "简单愉悦的智能生活体验",
+                "link": "https://apps.apple.com/cn/app/%E7%B1%B3%E5%AE%B6/id957323480",
+                "icon": "./images/MiHome.webp"
             }
         ]
     },
